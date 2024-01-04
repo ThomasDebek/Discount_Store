34fce47a006cad2b9e9ef4250ffa8bdd247f3ce3
ThomasDebek <tom.debek@gmail.com>
2024-01-04 23:54:49 +0100

    feat: category and brand

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,6 +6,9 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all
+    @products = @products.filter_by_category(params[:category]) if params[:category].present?
+    @products = @products.filter_by_brand(params[:brand]) if params[:brand].present?
+
   end
 
 
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,12 @@ class Product < ApplicationRecord
   has_one :user, through: :coupon
   belongs_to :user
   has_one_attached :image
+  belongs_to :category
+  belongs_to :brand
+
+  scope :filter_by_category, proc { |category_id| where(category_id: category_id) }
+  scope :filter_by_brand, proc { |brand_id| where(brand_id: brand_id )}
+
+
+
 end

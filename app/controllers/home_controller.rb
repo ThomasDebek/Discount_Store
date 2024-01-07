96bc416384c1d2c96ce311bee319af0db2dea034
ThomasDebek <tom.debek@gmail.com>
2024-01-07 22:21:25 +0100

    feat: comments and brand_category in main app

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,6 @@ class HomeController < ApplicationController
   def index
     @products = Product.all
     @products = @products.filter_by_category(params[:category]) if params[:category].present?
-    binding.pry
     @products = @products.filter_by_brand(params[:brand]) if params[:brand].present?
 
     @coupons = Coupon.all

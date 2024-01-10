07089bd857b9a4aee7ce7f23656fcbf5457bf624
ThomasDebek <tom.debek@gmail.com>
2024-01-10 22:56:39 +0100

    sort: add created_at: desc

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @products = Product.paginate(page: params[:page], per_page: 6)
+    @products = Product.order(created_at: :desc).paginate(page: params[:page], per_page: 6)
 
     @products = @products.filter_by_category(params[:category]) if params[:category].present?
     @products = @products.filter_by_brand(params[:brand]) if params[:brand].present?

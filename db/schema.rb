eaa17848d31f5ab91c607c654bfab57e69940d83
ThomasDebek <tom.debek@gmail.com>
2024-01-25 22:44:25 +0100

    feat: rails g migration add_quantity_to_cart_items quantity:integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_24_200614) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_25_213949) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
   enable_extension "pg_trgm"
@@ -56,6 +56,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_24_200614) do
     t.bigint "cart_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity", default: 1
     t.index ["cart_id"], name: "index_cart_items_on_cart_id"
     t.index ["product_id"], name: "index_cart_items_on_product_id"
   end

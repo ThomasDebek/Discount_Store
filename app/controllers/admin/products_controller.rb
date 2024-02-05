634c080413c55944c01abf38afa8e2688b18e621
ThomasDebek <tom.debek@gmail.com>
2024-02-05 13:53:20 +0100

    action: add new action in admin kontroller

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -9,4 +9,26 @@ class Admin::ProductsController < ApplicationController
     @products = @products.filter_by_brand(params[:brand]) if params[:brand].present?
   end
 
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      flash[:notice] = "Product added successfully"
+      redirect_to admin_products_path
+    else
+      render new
+    end
+  end
+
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :price, :category_id, :brand_id, :image)
+  end
+
 end

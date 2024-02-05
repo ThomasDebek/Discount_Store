562cfb26d1995163436f1058823fb062cdf8f397
ThomasDebek <tom.debek@gmail.com>
2024-02-05 13:15:00 +0100

    views: change navigation in user and admin

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -4,13 +4,9 @@ class Admin::ProductsController < ApplicationController
   before_action :authenticate_admin!
   def index
     " PANEL ADMIN"
-
     @products = Product.order(created_at: :desc).paginate(page: params[:page], per_page: 6)
-
     @products = @products.filter_by_category(params[:category]) if params[:category].present?
     @products = @products.filter_by_brand(params[:brand]) if params[:brand].present?
-
-    @show_sidebar = true
   end
 
 end

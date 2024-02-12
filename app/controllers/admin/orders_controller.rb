db415c6fde9ce822d1cb4e4fcf15d674eda58def
ThomasDebek <tom.debek@gmail.com>
2024-02-12 14:49:44 +0100

    feat: add delete order in admin panel

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,23 @@
 class Admin::OrdersController < ApplicationController
   layout 'admin'
+  before_action :find_order, only: %i[destroy]
   def index
     @orders = Order.order(created_at: :desc)
   end
+
+  def destroy
+    @order.destroy
+    flash[:notice] = "Order was been deleted"
+    redirect_to admin_order_path
+  end
+
+  private
+
+  def find_order
+    @order = Order.find(params[:id])
+  end
+  def order_params
+    params.require(:order).permit(:name)
+  end
+
 end

1f77cf97f005df93e59dd54c993104a2d4525b14
ThomasDebek <tom.debek@gmail.com>
2024-02-20 22:51:36 +0100

    feat: aasm

--- app/services/StateService.rb
+++ app/services/StateService.rb
@@ -1,39 +1,39 @@
 class StateService
-    include AASM
-    attr_reader :payment
+  include AASM
+  attr_reader :payment
 
-    def initialize(payment)
-      @payment = payment
-      if payment.state.nil?
-        update_payment_state
-      else
-        aasm.current_state = payment.state.to_sym
-      end
+  def initialize(payment)
+    @payment = payment
+    if payment.state.nil?
+      update_payment_state
+    else
+      aasm.current_state = payment.state.to_sym
+    end
+  end
+
+  aasm whiny_transitions: false do
+    state :pending, initial: true
+    state :failed, :completed
+
+    event :fail do
+      transitions from: :pending, to: :failed
     end
 
-    aasm whiny_transitions: false do
-      state :pending, initial: true
-      state :failed, :completed
-
-      event :fail do
-        transitions from: :pending, to: :failed
-      end
-
-      event :complete do
-        transitions from: :pending, to: :completed
-      end
-
-      after_all_transitions :update_payment_state
+    event :complete do
+      transitions from: :pending, to: :completed
     end
 
-    def possible_events
-      aasm.events(possible: true).map(&:name)
-    end
+    after_all_transitions :update_payment_state
+  end
 
-    private
+  def possible_events
+    aasm.events(possible: true).map(&:name)
+  end
 
-    def update_payment_state
-      payment.update!(state: aasm.to_state)
-    end
+  private
 
+  def update_payment_state
+    payment.update!(state: aasm.to_state)
+  end
 end
+

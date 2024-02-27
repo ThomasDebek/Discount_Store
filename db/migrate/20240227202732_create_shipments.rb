e79cd7be79029aacfbcf2b4ea1ac9c0c666022f2
ThomasDebek <tom.debek@gmail.com>
2024-02-27 21:29:27 +0100

    model: genratitin shipment

---
+++ db/migrate/20240227202732_create_shipments.rb
@@ -0,0 +1,10 @@
+class CreateShipments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :shipments do |t|
+      t.string :state
+      t.belongs_to :order, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_18_212514) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_27_202732) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
   enable_extension "pg_trgm"
@@ -134,6 +134,14 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_18_212514) do
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
+  create_table "shipments", force: :cascade do |t|
+    t.string "state"
+    t.bigint "order_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_shipments_on_order_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -175,4 +183,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_18_212514) do
   add_foreign_key "products", "brands"
   add_foreign_key "products", "categories"
   add_foreign_key "products", "users"
+  add_foreign_key "shipments", "orders"
 end
